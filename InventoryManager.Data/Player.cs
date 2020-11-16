50b68692af5a2d4bbfd9f57f36bb946254df9cf8
calfian <chrisalfian@knights.ucf.edu>
2020-11-16 12:22:41 -0500

    Tutorial 3

---
+++ InventoryManager.Data/Player.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace InventoryManager.Data
+{
+    public class Player : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+
+        public int Health { get; set; }
+
+        public int Score { get; set; }
+
+        [JsonProperty(PropertyName = "Inventory")]
+        private List<string> InventoryNames { get; set; }
+
+        [JsonIgnore]
+        public List<Item> Inventory { get; set; }
+
+
+        public Player()
+        {
+            InventoryNames = new List<string>();
+            Inventory = new List<Item>();
+        }
+
+        public void BuildInventoryFromName(List<Item> items)
+        {
+            Inventory = (from itemName in InventoryNames
+                         let item = items.Find(i => i.Name.Equals(itemName, System.StringComparison.InvariantCultureIgnoreCase))
+                         where item != null
+                         select item).ToList();     
+        }
+
+        public override string ToString() => Name;
+
+         
+
+    }
+}
---
+++ InventoryManager.Winforms/Forms/MainForm.cs
@@ -0,0 +1,123 @@
+﻿
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using InventoryManager.Data;
+using System.IO;
+using InventoryManager.Winforms.ViewModels;
+using InventoryManager.Winforms.Forms;
+using System.Reflection;
+using System.Linq;
+using System;
+
+namespace InventoryManager.Winforms.Forms
+
+{
+    public partial class MainForm : Form
+    {
+        public static string AssemblyTitle = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        private WorldViewModel ViewModel
+        {
+            get => mViewModel;
+            set
+            {
+                if (mViewModel != value)
+                {
+                    mViewModel = value;
+                    worldViewModelBindingSource.DataSource = mViewModel;
+                }
+            }
+        }
+
+
+        private bool IsWorldLoaded
+        {
+            get => mIsWorldLoaded;
+            set
+            {
+                mIsWorldLoaded = value;
+                mainTabControl.Enabled = mIsWorldLoaded;
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            ViewModel = new WorldViewModel();
+            IsWorldLoaded = false;
+        }
+
+        
+        private void playerNameTextBox_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void AddPlayerButton_Click(object sender, System.EventArgs e)
+        {
+            using (AddPlayerForm addPlayerForm = new AddPlayerForm())
+            {
+                if (addPlayerForm.ShowDialog() == DialogResult.OK)
+                {
+
+                    Player player = new Player { Name = addPlayerForm.PlayerName };
+                    ViewModel.Players.Add(player);
+                }
+            }
+        }
+
+        private void DeletePlayerButton_Click(object sender, System.EventArgs e)
+        {
+           if(MessageBox.Show("Delete this player?", AssemblyTitle, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                ViewModel.Players.Remove((Player)playersListBox.SelectedItem);
+                playersListBox.SelectedItem = ViewModel.Players.FirstOrDefault();
+            }
+        }
+
+        #region Main Menu
+
+        private void PlayersListBox_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            deletePlayerButton.Enabled = playersListBox.SelectedItem != null;
+        }
+        private void openWorldToolStripMenuItem1_Click(object sender, System.EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ViewModel.World = JsonConvert.DeserializeObject<World>(File.ReadAllText(openFileDialog.FileName));
+                ViewModel.Filename = openFileDialog.FileName;
+                IsWorldLoaded = true;
+            }
+        }
+        private void SaveToolStripMenuItem_Click(object sender, System.EventArgs e) => ViewModel.SaveWorld();
+
+        private void SaveAsToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            if(saveFileDialog.ShowDialog()== DialogResult.OK)
+            {
+                ViewModel.Filename = saveFileDialog.FileName;
+                ViewModel.SaveWorld();
+            }
+        }
+        private void ExitToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        #endregion Main Menu
+
+
+
+        private WorldViewModel mViewModel;
+        private bool mIsWorldLoaded;
+
+        private void playersTabPage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
